0ea18263f3e348237f69d5c357d3554ddd74959f
Parth <parth7k4@gmail.com>
2023-03-09 16:05:05 +0530

    bubble sort in 2 ways

---
+++ bubble.py
@@ -0,0 +1,16 @@
+my_list = [8, 10, 6, 2, 4]  # list to sort
+swapped = True  # It's a little fake, we need it to enter the while loop.
+
+#while swapped:
+#    swapped = False  # no swaps so far
+#    for i in range(len(my_list) - 1):
+#        if my_list[i] > my_list[i + 1]:
+#            swapped = True  # a swap occurred!
+#            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+
+print(my_list)
+for i in range(0, len(my_list)-1):
+    for j in range(i+1, len(my_list)):
+        if my_list[i]>my_list[j]:
+            my_list[i], my_list[j]=my_list[j], my_list[i]
+print(my_list)
