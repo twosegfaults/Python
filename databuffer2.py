92c92a343de511d51018cd2618cb429d6cf9f42a
Parth <parth7k4@gmail.com>
2023-04-04 15:18:14 +0530

    a variation on databuffer.py

---
+++ databuffer2.py
@@ -0,0 +1,26 @@
+#string=str(input("Enter the desired string:"))
+# I have entered the user-input method as comments
+input_file=open(r"input.txt", "r")
+line=input_file.readlines()
+
+def call():
+    for a in range(0, len(line)):
+        st=line[a]
+        x= pat(st)
+        print("The number of characters needed to be processed is:", x)
+    input_file.close()
+
+def pat(buffer):
+    for i in range(3, len(buffer)):
+        l=[]
+        string2=buffer[i-13:i+1]
+        for j in range(0,4):
+            if string2[j] not in l:
+                l.append(string2[j])
+        if len(l)==14:
+            return i+1
+    return -1
+
+call()
+#x= pat(string)
+#print("The number of characters needed to be processed is:", x)
